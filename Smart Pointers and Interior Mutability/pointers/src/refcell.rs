36e03a9c2df98dce670d56a645c8ffb97f67da32
Sifiso <sifiso1101@gmail.com>
2022-11-13 23:35:12 +0200

    Encapsulate 'Refstate' in Cell<T>
    This feature enables mutation of state via Cell<T> behind a shared reference

--- Smart Pointers and Interior Mutability/pointers/src/refcell.rs
+++ Smart Pointers and Interior Mutability/pointers/src/refcell.rs
@@ -8,34 +8,37 @@ The checks are done at runtime!
 */
 
 use std::cell::UnsafeCell;
+use crate::cell::Cell;
 
+#[derive(Copy, Clone)]
 enum RefState {
     Unshared, //T
     Shared(usize), //&T
     Exclusive, //&mut T
 }
 
+//Use cell to mutate behind a shared reference
 pub struct RefCell<T> {
     value: UnsafeCell<T>,
-    state: RefState,
+    state: Cell<RefState>,
 }
 
 impl<T> RefCell<T> {
     fn new(value: T) -> Self {
         Self {
             value: UnsafeCell::new(value),
-            state: RefState::Unshared,
+            state: Cell::new(RefState::Unshared),
         }
     }
 
     fn borrow(&self) -> Option<&T> {
-        match self.state {
+        match self.state.get() {
             RefState::Unshared => {
-                self.state = RefState::Shared(1);
+                self.state.set(RefState::Shared(1));
                 Some (unsafe {&*self.value.get()})
             },
             RefState::Shared(n) => {
-                self.state = RefState::Shared(n + 1);
+                self.state.set(RefState::Shared(n + 1));
                 Some(unsafe {&*self.value.get()})
             },
             _ => None ,
@@ -43,8 +46,8 @@ impl<T> RefCell<T> {
     }
 
     fn borrow_mut(&self) -> Option<&mut T> {
-        if let RefState::Unshared = self.state {
-            self.state = RefState::Exclusive;
+        if let RefState::Unshared = self.state.get() {
+            self.state.set(RefState::Exclusive);
             Some(unsafe {&mut *self.value.get()})
         } else {
             None
