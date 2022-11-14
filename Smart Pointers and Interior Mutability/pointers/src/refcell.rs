9cc87b4986b6ff35a1fefc0428d8878093140336
Sifiso <sifiso1101@gmail.com>
2022-11-14 20:28:05 +0200

    Rewrite refcell.rs

--- Smart Pointers and Interior Mutability/pointers/src/refcell.rs
+++ Smart Pointers and Interior Mutability/pointers/src/refcell.rs
@@ -1,31 +1,32 @@
 /*
 https://doc.rust-lang.org/std/cell/struct.RefCell.html:
 
-A mutable memory location with "safe" dynamically check borrow rules!
+A mutable memory location with "safe" dynamically checked borrow rules!
 
 The checks are done at runtime!
 
-*/
+Cell<T> will allow you to replace its value, but unlike RefCell<T>,
+it will not give you an exclusive reference (&mut) to that value.
 
-use std::cell::UnsafeCell;
-use crate::cell::Cell;
+*/
 
 #[derive(Copy, Clone)]
 enum RefState {
-    Unshared, //T
-    Shared(usize), //&T
-    Exclusive, //&mut T
+    Unshared,
+    Shared(usize),
+    Exclusive,
 }
 
-//Use cell to mutate behind a shared reference
+use crate::cell::Cell;
+use std::cell::UnsafeCell;
 pub struct RefCell<T> {
     value: UnsafeCell<T>,
     state: Cell<RefState>,
 }
 
 impl<T> RefCell<T> {
-    fn new(value: T) -> Self {
-        Self {
+    fn new(value: T) -> Self{
+        Self{
             value: UnsafeCell::new(value),
             state: Cell::new(RefState::Unshared),
         }
@@ -35,22 +36,21 @@ impl<T> RefCell<T> {
         match self.state.get() {
             RefState::Unshared => {
                 self.state.set(RefState::Shared(1));
-                Some (unsafe {&*self.value.get()})
-            },
-            RefState::Shared(n) => {
+                Some(unsafe { &*self.value.get() })
+            } RefState::Shared(n) => {
                 self.state.set(RefState::Shared(n + 1));
-                Some(unsafe {&*self.value.get()})
+                Some(unsafe { &*self.value.get() })
             },
-            _ => None ,
+            _ => None,
         }
     }
 
-    fn borrow_mut(&self) -> Option<&mut T> {
+    fn borrow_mut(&self) -> Option <&mut T> {
         if let RefState::Unshared = self.state.get() {
             self.state.set(RefState::Exclusive);
-            Some(unsafe {&mut *self.value.get()})
+            Some(unsafe { &mut *self.value.get() })
         } else {
             None
         }
     }
- }
+}
