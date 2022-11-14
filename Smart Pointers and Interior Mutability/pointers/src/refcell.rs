bc4a7244fbe585c644ea3fc504718ff03b6ecde6
Sifiso <sifiso1101@gmail.com>
2022-11-14 22:30:01 +0200

    Add Ref<T> and RefMut<T>
    Instead of giving of Option<&T> and Option<&mut T>,
    
    give out Option<Ref<'_, T>> and Option<RefMut<'_, T>>
    
    Add implementations for Drop and Deref traits

--- Smart Pointers and Interior Mutability/pointers/src/refcell.rs
+++ Smart Pointers and Interior Mutability/pointers/src/refcell.rs
@@ -32,25 +32,76 @@ impl<T> RefCell<T> {
         }
     }
 
-    fn borrow(&self) -> Option<&T> {
+    fn borrow(&self) -> Option<Ref<'_, T>> {
         match self.state.get() {
             RefState::Unshared => {
                 self.state.set(RefState::Shared(1));
-                Some(unsafe { &*self.value.get() })
+                Some(Ref {refcell: self})
             } RefState::Shared(n) => {
                 self.state.set(RefState::Shared(n + 1));
-                Some(unsafe { &*self.value.get() })
+                Some(Ref {refcell: self})
             },
             _ => None,
         }
     }
 
-    fn borrow_mut(&self) -> Option <&mut T> {
+    fn borrow_mut(&self) -> Option <RefMut<'_, T>> {
         if let RefState::Unshared = self.state.get() {
             self.state.set(RefState::Exclusive);
-            Some(unsafe { &mut *self.value.get() })
+            Some(RefMut {refcell: self})
         } else {
             None
         }
+    }
+}
+
+pub struct Ref<'refcell, T> {
+    refcell: &'refcell RefCell<T>,
+}
+
+use std::ops::{Deref, DerefMut};
+
+impl<T> Drop for Ref<'_, T> {
+    fn drop(&mut self) {
+        match self.refcell.state.get() {
+            RefState::Exclusive | RefState::Unshared => unreachable!(),
+            RefState::Shared(1) => self.refcell.state.set(RefState::Unshared),
+            RefState::Shared(n) => self.refcell.state.set(RefState::Shared(n-1)),
+        }
+    }
+}
+
+impl<T> Deref for Ref<'_, T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        unsafe { &*self.refcell.value.get() }
+    }
+}
+
+pub struct RefMut<'refcell, T> {
+    refcell: &'refcell RefCell<T>
+}
+
+impl<T> Drop for RefMut<'_, T> {
+    fn drop(&mut self) {
+        match self.refcell.state.get() {
+            RefState::Shared(_) | RefState::Unshared => unreachable!(),
+            RefState::Exclusive => self.refcell.state.set(RefState::Unshared),
+        }
+    }
+}
+
+impl<T> Deref for RefMut<'_, T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        unsafe { &*self.refcell.value.get() }
+    }
+}
+
+impl<T> DerefMut for RefMut<'_, T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        unsafe {&mut *self.refcell.value.get()}
     }
 }
