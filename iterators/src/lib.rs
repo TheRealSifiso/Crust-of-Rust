579ec2970cc562f1059340bb0db4843d0c2ec994
Sifiso <sifiso1101@gmail.com>
2022-12-07 17:06:11 +0200

    Add 'impl Iterator' for Flatten<O>

--- iterators/src/lib.rs
+++ iterators/src/lib.rs
@@ -2,16 +2,24 @@ pub fn flatten<O>(iter: O) -> Flatten<O> {
     Flatten::new(iter)
 }
 
-pub struct Flatten<O>{
+pub struct Flatten<O> {
     outer: O,
 }
 
 impl<O> Flatten<O> {
     fn new(iter: O) -> Self {
-        Self {
-            outer: iter,
-        }
+        Self { outer: iter }
     }
+}
+
+impl<O> Iterator for Flatten<O>
+where
+    O: Iterator,
+    O::Item: IntoIterator,
+{
+    type Item = <O::Item as IntoIterator>::Item;
+
+    fn next(&mut self) -> Option<Self::Item> {}
 }
 
 /*
