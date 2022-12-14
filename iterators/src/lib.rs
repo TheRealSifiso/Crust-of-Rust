326afd1305d255cb10b5b0b649fe0ddb065b767d
Sifiso <sifiso1101@gmail.com>
2022-12-14 12:11:26 +0200

    Rename 'newtypeiter.rs' to 'newtype.rs'

--- iterators/src/lib.rs
+++ iterators/src/lib.rs
@@ -1,1 +1,2 @@
 mod flatten;
+mod newtype;
--- iterators/target/debug/deps/iterators-b11ccc5879b427fc.d
+++ iterators/target/debug/deps/iterators-b11ccc5879b427fc.d
@@ -1,6 +1,7 @@
-c:\Users\Sifiso\Documents\Crust-of-Rust\iterators\target\debug\deps\iterators-b11ccc5879b427fc.rmeta: src\lib.rs src\flatten.rs
+c:\Users\Sifiso\Documents\Crust-of-Rust\iterators\target\debug\deps\iterators-b11ccc5879b427fc.rmeta: src\lib.rs src\flatten.rs src\newtype.rs
 
-c:\Users\Sifiso\Documents\Crust-of-Rust\iterators\target\debug\deps\iterators-b11ccc5879b427fc.d: src\lib.rs src\flatten.rs
+c:\Users\Sifiso\Documents\Crust-of-Rust\iterators\target\debug\deps\iterators-b11ccc5879b427fc.d: src\lib.rs src\flatten.rs src\newtype.rs
 
 src\lib.rs:
 src\flatten.rs:
+src\newtype.rs:
