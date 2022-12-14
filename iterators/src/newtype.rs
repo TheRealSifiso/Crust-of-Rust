cd39376c7d1bd892ddf4e188801875b09e0ea6d5
Sifiso <sifiso1101@gmail.com>
2022-12-14 12:17:44 +0200

    Implement 'Iterator' trait for 'NewTypeIter<'a, T>'

--- iterators/src/newtype.rs
+++ iterators/src/newtype.rs
@@ -13,3 +13,16 @@ struct NewTypeIter<'a, T> {
     inner: &'a NewType<T>,
     pos: usize,
 }
+
+impl<'a, T> Iterator for NewTypeIter<'a, T>{
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.pos >= self.inner.0.len() {
+            self.pos += 1;
+            return self.inner.0.get(self.pos - 1);
+        }
+
+        None
+    }
+}
