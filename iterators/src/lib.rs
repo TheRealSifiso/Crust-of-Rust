7ef713c1f187384e4dc04ce8e544496d67bf094b
Sifiso <sifiso1101@gmail.com>
2022-12-15 20:31:31 +0200

    Implement Iterator for Flatten<O>

--- iterators/src/lib.rs
+++ iterators/src/lib.rs
@@ -1,25 +1,23 @@
 fn flatten<O>(iter: O) -> Flatten<O> {
-    Flatten { outer: iter }
+    Flatten { outer_iter: iter }
 }
 
 struct Flatten<O> {
-    outer: O,
+    outer_iter: O,
 }
 
 impl<O> Iterator for Flatten<O>
 where
     O: Iterator,
-    O::Item: Iterator,
+    O::Item: IntoIterator,
 {
     type Item = <O::Item as IntoIterator>::Item;
 
+    // 'O' -> outer iterator
+    // 'O::Item' -> collection yielded by the call to next() on 'O' (the 'outer' iterator)
+    // 
+
     fn next(&mut self) -> Option<Self::Item> {
-        todo!()
+          self.outer_iter.next().and_then(|inner_collection| inner_collection.into_iter().next())
     }
 }
-
-/*
-
-let vec = vec![vec[]!, vec[]!]
-
-*/
