845616a10dcf09919deaefc71bbf79fa6b53903d
Sifiso <sifiso1101@gmail.com>
2022-12-16 00:42:50 +0200

    Fix 'Iterator' and 'DoubleEndedIterator' implementations

--- iterators/src/lib.rs
+++ iterators/src/lib.rs
@@ -5,7 +5,8 @@ where
 {
     Flatten {
         outer_iter: iter,
-        inner_iter: None,
+        forward_iter: None,
+        backward_iter: None,
     }
 }
 
@@ -15,7 +16,8 @@ where
     <O as Iterator>::Item: IntoIterator,
 {
     outer_iter: O,
-    inner_iter: Option<<O::Item as IntoIterator>::IntoIter>,
+    forward_iter: Option<<O::Item as IntoIterator>::IntoIter>,
+    backward_iter: Option<<O::Item as IntoIterator>::IntoIter>,
 }
 
 impl<O> Iterator for Flatten<O>
@@ -31,17 +33,21 @@ where
 
     fn next(&mut self) -> Option<Self::Item> {
         loop {
-            //Check whether inner iter exists
-            if let Some(ref mut inner_iter) = self.inner_iter {
-                //Check whether inner iter has been exhausted, or has any items to yield
-                if let Some(item) = inner_iter.next() {
+            //Check if forward_inner_iter exists
+            if let Some(ref mut forward_inner_iter) = self.forward_iter {
+                //Check if there are any items to be yielded by the forward_inner_iter
+                if let Some(item) = forward_inner_iter.next() {
                     return Some(item);
                 }
 
-                self.inner_iter = None;
+                self.forward_iter = None;
             }
 
-            self.inner_iter = Some(self.outer_iter.next()?.into_iter());
+            if let Some(inner_collection) = self.outer_iter.next() {
+                self.forward_iter = Some(inner_collection.into_iter());
+            } else {
+                return self.backward_iter.as_mut()?.next();
+            }
         }
     }
 }
@@ -54,17 +60,21 @@ where
 {
     fn next_back(&mut self) -> Option<Self::Item> {
         loop {
-            //Check whether inner iter exists
-            if let Some(ref mut inner_iter) = self.inner_iter {
-                //Check whether inner iter has been exhausted, or has any items to yield
-                if let Some(item) = inner_iter.next_back() {
+            //Check if forward_inner_iter exists
+            if let Some(ref mut backward_inner_iter) = self.backward_iter {
+                //Check if there are any items to be yielded by the forward_inner_iter
+                if let Some(item) = backward_inner_iter.next() {
                     return Some(item);
                 }
 
-                self.inner_iter = None;
+                self.backward_iter = None;
             }
 
-            self.inner_iter = Some(self.outer_iter.next_back()?.into_iter());
+            if let Some(inner_collection) = self.outer_iter.next() {
+                self.backward_iter = Some(inner_collection.into_iter());
+            } else {
+                return self.forward_iter.as_mut()?.next();
+            }
         }
     }
 }
@@ -105,17 +115,27 @@ mod tests {
     }
 
     #[test]
-    fn reverse(){
-        assert_eq!(flatten(std::iter::once(vec!["a", "b"])).rev().collect::<Vec<_>>(), vec!["b", "a"]);
+    fn reverse() {
+        assert_eq!(
+            flatten(std::iter::once(vec!["a", "b"]))
+                .rev()
+                .collect::<Vec<_>>(),
+            vec!["b", "a"]
+        );
     }
 
     #[test]
-    fn reverse_wide(){
-        assert_eq!(flatten(vec![vec!["a"], vec!["b"]].into_iter()).rev().collect::<Vec<_>>(), vec!["b", "a"]);
+    fn reverse_wide() {
+        assert_eq!(
+            flatten(vec![vec!["a"], vec!["b"]].into_iter())
+                .rev()
+                .collect::<Vec<_>>(),
+            vec!["b", "a"]
+        );
     }
 
     #[test]
-    fn both_ends(){
+    fn both_ends() {
         let mut iter = flatten(vec![vec!["a1", "a2", "a3"], vec!["b1", "b2", "b3"]].into_iter());
 
         assert_eq!(iter.next(), Some("a1"));
