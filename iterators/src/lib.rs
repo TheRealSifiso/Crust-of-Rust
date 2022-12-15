8c3a648cf3c5c2bf2f4b492283e67dc16ced61b5
Sifiso <sifiso1101@gmail.com>
2022-12-15 22:42:39 +0200

    Fix implementation of 'Iterator' for Flatten<O>

--- iterators/src/lib.rs
+++ iterators/src/lib.rs
@@ -1,9 +1,21 @@
-fn flatten<O>(iter: O) -> Flatten<O> {
-    Flatten { outer_iter: iter }
+fn flatten<O>(iter: O) -> Flatten<O>
+where
+    O: Iterator,
+    O::Item: IntoIterator,
+{
+    Flatten {
+        outer_iter: iter,
+        inner_iter: None,
+    }
 }
 
-struct Flatten<O> {
+struct Flatten<O>
+where
+    O: Iterator,
+    <O as Iterator>::Item: IntoIterator,
+{
     outer_iter: O,
+    inner_iter: Option<<O::Item as IntoIterator>::IntoIter>,
 }
 
 impl<O> Iterator for Flatten<O>
@@ -15,10 +27,22 @@ where
 
     // 'O' -> outer iterator
     // 'O::Item' -> collection yielded by the call to next() on 'O' (the 'outer' iterator)
-    // 
+    // [[], [], []]
 
     fn next(&mut self) -> Option<Self::Item> {
-          self.outer_iter.next()?.into_iter().next()
+        loop {
+            //Check whether inner iter exists
+            if let Some(ref mut inner_iter) = self.inner_iter {
+                //Check whether inner iter has been exhausted, or has any items to yield
+                if let Some(item) = inner_iter.next() {
+                    return Some(item);
+                }
+
+                self.inner_iter = None;
+            }
+
+            self.inner_iter = Some(self.outer_iter.next()?.into_iter());
+        }
     }
 }
 
@@ -28,22 +52,25 @@ mod tests {
     use super::*;
 
     #[test]
-    fn empty(){
+    fn empty() {
         assert_eq!(flatten(std::iter::empty::<Vec<()>>()).count(), 0);
     }
 
     #[test]
-    fn empty_wide(){
-        assert_eq!(flatten(vec![Vec::<()>::new(), vec![], vec![]].into_iter()).count(), 0);
+    fn empty_wide() {
+        assert_eq!(
+            flatten(vec![Vec::<()>::new(), vec![], vec![]].into_iter()).count(),
+            0
+        );
     }
 
     #[test]
-    fn one(){
-        assert_eq!(flatten(std::iter::once(vec!["a", "b", "c"])).count(), 1);
+    fn one() {
+        assert_eq!(flatten(std::iter::once(vec!["a", "b", "c"])).count(), 3);
     }
 
     #[test]
-    fn two(){
+    fn two() {
         assert_ne!(flatten(std::iter::once(vec!["a", "b", "c"])).count(), 2);
     }
 
@@ -51,5 +78,4 @@ mod tests {
     fn two_wide() {
         assert_eq!(flatten(vec![vec!["a"], vec!["b"]].into_iter()).count(), 2);
     }
-
 }
